a76a9c7ad2f9d50c704f10e1e071e764916e5d9e
krigga <krigga@migga.com>
2024-11-01 09:13:54 +0200

    .\js-scripts\

---
+++ js-scripts/arrays-loops.js
@@ -0,0 +1,19 @@
+for (let i =11; i <=20; i++){
+    console.log(i);}
+
+for (let i =20; i >=11; i--){
+    console.log(i);}
+    
+let friends = ["Sigma", "Ligma", "Enigma", "Alpha", "Alma"]
+    console.log(friends)
+    console.log(friends[0])
+    console.log(friends[2])
+    
+    
+    let friendsCount = friends.length;
+    console.log(friendsCount);
+
+
+    for (let i = 0; i < friends.length; i++) {
+        console.log(`${i + 1}. ${friends[i]}`); 
+    }
---
+++ js-scripts/variables.js
@@ -0,0 +1,12 @@
+let name = "Tavs Vārds";
+
+let age = 25; 
+
+let isStudent = true;
+
+const PI = 3.14;
+
+console.log(name);     
+console.log(age);      
+console.log(isStudent); 
+console.log(PI);  
